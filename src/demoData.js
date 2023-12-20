f839cf3afe939e5fcfe958fe004d5de586d2433c
kanja23 <martinkaranja92@gmail.com>
2023-12-20 04:53:58 -0800

    demoData.js

---
+++ src/demoData.js
@@ -0,0 +1,65 @@
+import conserve1.png from './assets/img/conserve1.png'
+import conserve2.png from './assets/img/conserve2.png'
+import conserve3.jpeg from './assets/img/conserve3.jpeg'
+import conserve4.jpeg from './assets/img/conserve4.jpeg'
+import conserve5.jpeg from './assets/img/conserve5.jpeg'
+import conserve6.png from './assets/img/conserve6.png'
+
+export const sliderImg = [
+  {
+    id: 1,
+    photo: conserve1.png,
+  },
+  {
+    id: 2,
+    photo: conserve2.png,
+  },
+  {
+    id: 3,
+    photo: conserve3.jpeg,
+  },
+  {
+    id: 4,
+    photo: conserve4.jpeg,
+  },
+  {
+    id: 5,
+    photo: conserve5.jpeg,
+  },
+  {
+    id: 6,
+    photo: conserve6.png,
+  },
+]
+
+export const cardInfo = [
+{
+    title: "Learn Energy Efficiency Basics",
+    description: "Explore the fundamental principles of energy efficiency and why it matters for a sustainable future.",
+  },
+  {
+    title: "Implement Energy-Saving Practices",
+    description: "Discover actionable tips and practices to save energy in your daily life and reduce environmental impact.",
+  },
+  {
+    title: "Switch to Renewable Energy",
+    description: "Consider transitioning to renewable energy sources to contribute to a cleaner and greener energy ecosystem.",
+  },
+  {
+    title: "Upgrade to Energy-Efficient Appliances",
+    description: "Explore the benefits of using energy-efficient appliances and how they can positively impact your energy consumption.",
+  },
+  {
+    title: "Participate in Community Initiatives",
+    description: "Get involved in local and community-driven energy efficiency initiatives to make a collective impact.",
+  },
+  {
+    title: "Monitor Your Carbon Footprint",
+    description: "Learn how to measure and reduce your carbon footprint for a more sustainable and energy-efficient lifestyle.",
+  },
+];
+  `Relax and have us come do all the work. Schedule a pickup today and we'd be glad to head right up`,
+  ` Find a dropoff center around you and deliver your recyclable waste locally`,
+  `Earn KLIN points everytime you donate trash`,
+  `Get up to 100 KLIN points within 2 months and have your points converted to a cash or gift price, ready for redemption `,
+]
